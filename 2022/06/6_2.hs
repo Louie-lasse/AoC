f2684c1284116414ea1998f050ac9635cb5c3150
Louie-lasse <love.lindqvist101@gmail.com>
2022-12-25 21:34:42 +0100

    separates part 1 and 2

--- 2022/06/6_2.hs
+++ 2022/06/6_2.hs
@@ -4,7 +4,7 @@ main :: IO ()
 main = print . value =<< readFile "text.txt"
 
 value :: String -> Int
-value s | length ff == length (nub ff) = 4
+value s | length ff == length (nub ff) = 14
         | otherwise                    = 1 + value (tail s)
         where
-            ff = take 4 s
+            ff = take 14 s
