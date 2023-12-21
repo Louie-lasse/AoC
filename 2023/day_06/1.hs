32312b2e936d120e2b617230bade5e2f417da755
Louie-lasse <love.lindqvist101@gmail.com>
2023-12-21 19:36:46 +0100

    completes day 6

--- 2023/day_06/1.hs
+++ 2023/day_06/1.hs
@@ -14,3 +14,30 @@ splitWhen f as = map reverse $ splitWhen' f as [] where
                             | otherwise = splitWhen' f as (a:cum)
     splitWhen' _ _      cum | null cum  = []
                             | otherwise = [cum]
+
+type Race = (Integer,Integer)
+
+parse :: [String] -> [Race]
+parse [t,d] | (t',d') <- both (filter (not . null) . splitWhen (==' ') . dropWhile (not . isDigit)) (t,d)
+            = zip (map read t') (map read d')
+
+main = readFile "input.txt" >>= print . product . map alternatives . parse . lines
+
+alternatives :: Race -> Integer
+alternatives r | (l,h) <- alt' r = toInteger $ h-l+1 where
+    alt' :: Integral a => Race -> (a,a)
+    alt' r | (t,d) <- both fromInteger r
+           , delta <- sqrt (t*t-4*d) / 2
+           , top <- t / 2
+           = (ceiling' (top-delta) , floor' (top+delta))
+
+ceiling' :: (RealFrac a1, Integral a2) => a1 -> a2
+ceiling' f | rounded f = ceiling f + 1
+           | otherwise = ceiling f
+
+floor' :: (RealFrac a1, Integral a2) => a1 -> a2
+floor' f   | rounded f = floor f - 1
+           | otherwise = floor f
+
+rounded :: RealFrac a => a -> Bool
+rounded f = floor f == ceiling f
